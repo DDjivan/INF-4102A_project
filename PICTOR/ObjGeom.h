1b9659509ba3b49c39d6dbf2a9b68b8aafeea13c
δDD <64313188+DDjivan@users.noreply.github.com>
2025-12-01 19:56:27 +0100

    feat(etape8): permet de sérialiser tous les ObjGeom

--- PICTOR/ObjGeom.h
+++ PICTOR/ObjGeom.h
@@ -5,6 +5,7 @@
 #include "V2.h"
 #include "ObjAttr.h"
 #include "Graphics.h"
+#include <sstream>
 
 class ObjGeom
 {
@@ -17,6 +18,37 @@ public :
 	virtual void draw(Graphics & G) {}
 
 	virtual void getHitbox(V2& P, V2& size){}
+
+	virtual std::string Serialize() const = 0;
+
+	std::string drawInfoSerialize() const
+	{
+		float R, G, B, A;
+
+		std::stringstream ss;
+
+		ss << "drawInfo_:{";
+
+		R = drawInfo_.borderColor_.R;
+		G = drawInfo_.borderColor_.G;
+		B = drawInfo_.borderColor_.B;
+		A = drawInfo_.borderColor_.A;
+		ss << "borderColor_:(" << R << "," << G << "," << B << "," << A << "),";
+
+		R = drawInfo_.interiorColor_.R;
+		G = drawInfo_.interiorColor_.G;
+		B = drawInfo_.interiorColor_.B;
+		A = drawInfo_.interiorColor_.A;
+		ss << "interiorColor_:(" << R << "," << G << "," << B << "," << A << "),";
+
+		ss << "thickness_:" << drawInfo_.thickness_ << ",";
+
+		ss << "isFilled_:" << drawInfo_.isFilled_ << "";
+
+		ss << "}";
+
+		return ss.str();
+	}
 };
 
 
@@ -43,10 +75,25 @@ public :
 		G.drawRectangle(P, size, drawInfo_.borderColor_, false, drawInfo_.thickness_);
 	}
 
-	void getHitbox(V2& P, V2& size) override 
+	void getHitbox(V2& P, V2& size) override
 	{
 		getPLH(P1_, P2_, P, size);
 	}
+
+	std::string Serialize() const override
+    {
+        std::stringstream ss;
+
+        ss << "ObjRectangle:{";
+
+		ss << drawInfoSerialize() << ",";
+
+		ss << "P1_:" << P1_ << ",";
+		ss << "P2_:" << P2_ << "";
+		ss << "}";
+
+        return ss.str();
+    }
 };
 
 
@@ -65,14 +112,29 @@ public:
 	void draw(Graphics& G) override
 	{
 		V2 P, size;
-	 
+
 		G.drawLine(P1_, P2_, drawInfo_.borderColor_, drawInfo_.thickness_);
 	}
 
-	void getHitbox(V2& P, V2& size) override 
+	void getHitbox(V2& P, V2& size) override
 	{
 		getPLH(P1_, P2_, P, size);
 	}
+
+	std::string Serialize() const override
+    {
+        std::stringstream ss;
+
+        ss << "ObjSegment:{";
+
+		ss << drawInfoSerialize() << ",";
+
+		ss << "P1_:" << P1_ << ",";
+		ss << "P2_:" << P2_ << "";
+		ss << "}";
+
+        return ss.str();
+    }
 };
 
 
@@ -97,10 +159,25 @@ public:
 	}
 
 
-	void getHitbox(V2& P, V2& size) override 
+	void getHitbox(V2& P, V2& size) override
 	{
 		float r = (P1_ - P2_).norm();
 		size = V2(2 * r, 2 * r);
 		P = P1_ - V2(r, r);
 	}
+
+	std::string Serialize() const override
+    {
+        std::stringstream ss;
+
+        ss << "ObjCercle:{";
+
+		ss << drawInfoSerialize() << ",";
+
+		ss << "P1_:" << P1_ << ",";
+		ss << "P2_:" << P2_ << "";
+		ss << "}";
+
+        return ss.str();
+    }
 };
